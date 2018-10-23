ae30a38c19cf20b132703584117504a09d2975c5
Kelli <kelli@Kellis-MacBook-Pro.local>
2018-10-23 16:16:38 -0400

    piggyBank and Karma Commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,16 @@ import SignupPage from "./pages/SignupPage";
 import HomePage from "./pages/HomePage";
 import GetSmarterPage from "./pages/GetSmarterPage";
 import NoMatch from "./pages/NoMatch";
+import piggyBank from "./pages/piggyBank";
+import goodKarma from "./pages/goodKarma";
 
 
 
 
 const App = () => (
+
+  
+
   <Router>
     <div>
       <Switch>
@@ -17,10 +22,15 @@ const App = () => (
         <Route exact path="/signuppage" component={SignupPage} />
         <Route exact path="/homepage" component={HomePage} />
         <Route exact path="/getsmarterpage" component={GetSmarterPage} />
+        <Route exact path="/piggyBankpage" component={piggyBank} />
+        <Route exact path="/goodKarmapage" component={goodKarma} />
         <Route component={NoMatch} />
+        
       </Switch>
     </div>
   </Router>
 );
+
+
 
 export default App;
